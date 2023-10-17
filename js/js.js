fbd74711a514578f52e30bb8be21c43632f1a572
PAinlife123 <148286364+PAinlife123@users.noreply.github.com>
2023-10-18 06:13:24 +0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,7 @@
-document.getElementById("myBtn").addEventListener("click", displayDate);
 
-function displayDate() {
-  document.getElementById("demo").disabled = true;
+function myFunction() {
+
+  document.getElementById("myText").disabled = true;
+
 }
+
